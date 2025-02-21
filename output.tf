74c27900bdb37abe94c7d05044a504c1bed45026
ayon2016 <ayon.choudhury2016@gmail.com>
2025-02-20 19:47:12 -0500

    modified code

--- output.tf
+++ output.tf
@@ -1,7 +1,11 @@
-output "public_ips" {
-  value = [azurerm_public_ip.public_ip1.ip_address, azurerm_public_ip.public_ip2.ip_address]
+output "locust_master_public_ip" {
+  value = azurerm_public_ip.public_ip1.ip_address
 }
 
-output "private_ips" {
-  value = [azurerm_network_interface.nic1.private_ip_address, azurerm_network_interface.nic2.private_ip_address]
+output "locust_worker_public_ip" {
+  value = azurerm_public_ip.public_ip2.ip_address
+}
+
+output "locust_master_private_ip" {
+  value = azurerm_network_interface.nic1.private_ip_address
 }
